0bb04a229ee3c8ddc8c0f646f718cfd2892176b0
Fabian Friederichs <fabian.friederichs@gmail.com>
2020-01-02 21:44:29 +0100

    stuff

---
+++ Part2Geometry/include/symmetry.h
@@ -0,0 +1,17 @@
+#ifndef _SYMMETRY_H_
+#define _SYMMETRY_H_
+#include <memory>
+#include <Eigen/Dense>
+#include <limits>
+
+
+class SymmetryDetector
+{
+public:
+	
+
+	
+private:	
+};
+
+#endif
---
+++ Part2Geometry/src/symmetry.cpp
@@ -0,0 +1,7 @@
+#include <symmetry.h>
+#include <Eigen/Core>
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+#include <icp.h>
+
